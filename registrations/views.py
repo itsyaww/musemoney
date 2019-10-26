c5d6196975be531e04df0c8e02b1426cda1c52b2
Eddie-MG <zccaeme@ucl.ac.uk>
2019-10-26 17:45:35 +0100

    page config

--- registrations/views.py
+++ registrations/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from django.views import generic
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'registrations/index.html')
