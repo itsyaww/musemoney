f1b9e4a4e1b6c3e6ea3d92fd15b9d47bd2152801
SawdaO <sawda.onanuga@gmail.com>
2019-10-26 20:11:01 +0100

    some stuff

--- registrations/models.py
+++ registrations/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Investor(models.Model):
+    email = models.CharField()
+    f_name = models.CharField() 
+    l_name =models.CharField() 
+    u_rname = models.CharField()
+    capital = models.CharField()
+class Artist(models.Model):
+    email = models.CharField()
+    f_name = models.CharField() 
+    l_name =models.CharField() 
+    u_rname = models.CharField()
+    bio = models.CharField()
+    twit_f = models.CharField()
+    fb_f = models.CharField()
+    yt_f = models.CharField()
+    spotify_f = models.CharField()
+    soundcloud_f = models.CharField()
+    insta_f = models.CharField()
+class asset(models.Model):
+    
